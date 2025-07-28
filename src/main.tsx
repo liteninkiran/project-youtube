eaeed270e3f2abff7fb95c1ac699229ca37a957a
Kiran Anand <liteninkiran@hotmail.com>
2025-07-28 11:16:42 +0100

    Ignore front-end routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,21 @@ import App from './App.tsx';
 
 if (import.meta.env.DEV) {
     const { worker } = await import('./mocks/browser');
-    worker.start();
+    // worker.start();
+
+    worker.start({
+        onUnhandledRequest(request) {
+            // Ignore front-end route navigation requests
+            if (request.destination === '' && request.mode === 'navigate') {
+                return;
+            }
+
+            // Log other unhandled requests
+            console.warn(
+                `[MSW] Warning: Unhandled ${request.method} request to ${request.url}`
+            );
+        },
+    });
 }
 
 createRoot(document.getElementById('root')!).render(
